f30fb88e4d0970d58e8f3d64e246a5ba6243a05b
Ilyas Salhi <ilyassalhi@gmail.com>
2019-04-03 19:16:20 -0400

    Added my esc test ino for my drone

---
+++ escTest.ino
@@ -0,0 +1,54 @@
+#include <ESP32Servo.h>
+#include "config.h"
+// ---------------------------------------------------------------------------
+Servo motA, motB, motC, motD;
+String data;
+/**
+ * Start serial, attach motors, and display the instructions and format
+ */
+void setup() {
+    Serial.begin(115200);
+    motA.attach(UPPER_LEFT_MOTOR, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    motB.attach(UPPER_RIGHT_MOTOR, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    motC.attach(LOWER_LEFT_MOTOR, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    motD.attach(LOWER_RIGHT_MOTOR, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+    giveInstructions();
+}
+
+
+/**
+ * Loop: Read input and execute instruction
+ */
+void loop() {
+    if (Serial.available()) {
+        data = Serial.readString();
+        char motor = data.charAt(0);
+        int time = data.substring(2).toInt();
+        if(motor == 'a'){
+            motA.writeMicroseconds(time);    
+        }else if(motor == 'b'){
+            motB.writeMicroseconds(time);
+        }else if(motor == 'c'){
+            motC.writeMicroseconds(time);
+        }else if(motor == 'd'){
+            motD.writeMicroseconds(time);
+        }else if(motor == 'f'){
+            motA.writeMicroseconds(time); 
+            motB.writeMicroseconds(time); 
+            motC.writeMicroseconds(time); 
+            motD.writeMicroseconds(time); 
+        }
+    }
+}
+
+/**
+ * Give user control format
+ */
+void giveInstructions()
+{  
+    Serial.println("INTERFACE RULES:");
+    Serial.println("\tINPUT MUST BE -> MOTOR_LETTER.PULSE_LENGTH");
+    Serial.println("\t\tMOTOR_LETTER = {a, b, c, d, f}");
+    Serial.println("\t\tf is for all motors at the same time");
+    Serial.println("\t\tPULSER_LENGTH = [1000,2000]");
+}
